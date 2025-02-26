775b37404ee8ab7a2539eb4ea0efc4b2ede1c534
alibabamtk62 <alibabamtk62@gmail.com>
2025-02-26 09:03:49 +0800

    Add files via upload

---
+++ src/contentData/content.js
@@ -0,0 +1,36 @@
+const productData = [
+    {
+      id: 1,
+      name: "MTK AIO",
+      image:
+        "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+      timeLeft: 27,
+      appLink: "https://panelvip.co.uk/MTK_AIO.apk"
+    },
+    {
+      id: 2,
+      name: "MTK GENUINE PRO",
+      image:
+        "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY"],
+      rating: 5,
+      timeLeft: 24,
+      appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.MtkGenuineProV2"
+    }
+   // {
+   //   id: 3,
+   //   name: "STARLINK VPN LOGIN",
+   //   image:
+   //     "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
+   //   productType: "openBackHeadphones",
+   //   description: ["OVPN", "UDP", "V2RAY"],
+   //   rating: 5,
+   //   timeLeft: 27,
+   //   appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.StarlinkVPNv2"
+   // }
+  ];
+  export default productData;
