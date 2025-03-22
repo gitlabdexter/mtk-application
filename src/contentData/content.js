c1e5545c9ec47f205fa4b3e7cad33445dc4cf348
MTK APPLICATION <alibabamtk62@gmail.com>
2025-03-22 21:37:22 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -7,7 +7,7 @@ const productData = [
       description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
       rating: 5,
       timeLeft: 27,
-      appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.MtkGenuineProV2"
+      appLink: "https://panelvip.co.uk/1.2-release.apk"
     },
     {
       id: 2,
