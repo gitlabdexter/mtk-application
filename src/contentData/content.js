457a68c60c588a83c1b70e5c0000febf9b480edd
MTK APPLICATION <alibabamtk62@gmail.com>
2025-03-23 14:24:28 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -7,7 +7,7 @@ const productData = [
       description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
       rating: 5,
       timeLeft: 27,
-      appLink: "https://panelvip.co.uk/sdk35-release.apk"
+      appLink: "https://panelvip.co.uk/fix-115-release.apk"
     },
     {
       id: 2,
