21d2ad9f425660a45aaf4615a8efab97a1b4dbbe
MTK APPLICATION <alibabamtk62@gmail.com>
2025-04-23 07:22:46 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -17,7 +17,7 @@ const productData = [
       description: ["OVPN", "UDP", "V2RAY"],
       rating: 5,
       timeLeft: 24,
-      appLink: "https://adminvpn.site/mtk-release.apk"
+      appLink: "https://adminvpn.site/appmtk.apk"
     },
     {
       id: 2,
