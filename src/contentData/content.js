5b340263a56718e5df0c5b24d4f6408c1798db53
gitlabdexter <gitlabdexter@gmail.com>
2025-09-04 17:49:23 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -8,26 +8,26 @@ const productData = [
       rating: 5,
       timeLeft: 27,
       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/fix-119-release.apk"
-    },
-    {
-      id: 2,
-      name: "MTK GENUINE VIP",
-      image:"https://i.ibb.co/0RffdrVp/app-main-logo.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkgenuine-release.apk"
-    },
-    {
-      id: 3,
-      name: "MTK RM",
-      image:"https://i.ibb.co/qLsDNCVN/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkrm-release.apk"
+   // },
+   // {
+   //   id: 2,
+   //   name: "MTK GENUINE VIP",
+   //   image:"https://i.ibb.co/0RffdrVp/app-main-logo.png",
+   //   productType: "openBackHeadphones",
+   //   description: ["OVPN", "UDP", "V2RAY"],
+   //   rating: 5,
+   //   timeLeft: 24,
+   //   appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkgenuine-release.apk"
+   // },
+   // {
+   //   id: 3,
+   //   name: "MTK RM",
+   //   image:"https://i.ibb.co/qLsDNCVN/icon-icon.png",
+   //   productType: "openBackHeadphones",
+    //  description: ["OVPN", "UDP", "V2RAY"],
+    //  rating: 5,
+    //  timeLeft: 24,
+    //  appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkrm-release.apk"
     }
    // {
    //   id: 3,
