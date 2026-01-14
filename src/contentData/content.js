4aa0303b425ee1a87146aa584e3ccdc93bd3934c
gitlabdexter <gitlabdexter@gmail.com>
2026-01-14 12:45:58 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,13 +1,13 @@
 const productData = [
     {
       id: 1,
-      name: "MTK GENUINE",
-      image:"https://i.ibb.co/WNt2NMRb/icon-icon.png",
+      name: "MTK GENUINE PRO",
+      image:"https://i.ibb.co/qLsDNCVN/icon-icon.png",
       productType: "openBackHeadphones",
       description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
       rating: 5,
       timeLeft: 27,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtk-release.apk"
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/progenuine.apk"
     },
     {
       id: 2,
