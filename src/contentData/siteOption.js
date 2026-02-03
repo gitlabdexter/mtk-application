7150c6a67447f51bdb6d2ce320814c3f1f8fca3b
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 14:08:51 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -3,7 +3,7 @@ const siteOptionData = [
       id: 1,
       siteName: "MEDIATEK APPLICATION",
       image:
-        "https://i.ibb.co/jPnXFqj5/icon-icon.png",
+        "https://i.ibb.co/dsBK8FTw/app-main-logo.png",
       description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
       siteLink: "https://sumsag-ultra.site/"
     }
