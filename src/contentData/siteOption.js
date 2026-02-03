d78c6dd5d19ae92008b36a706d2744d467de9844
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:46:22 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -1,11 +1,12 @@
-const siteOptionData = [
-    {
-      id: 1,
-      siteName: "MEDIATEK APPLICATION",
-      image:
-        "https://i.ibb.co/qLsDNCVN/icon-icon.png",
-      description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
-      siteLink: "https://mtk-genuine.com/"
-    }
-  ];
-  export default siteOptionData;
+// src/contentData/siteOption.js
+const siteOptions = [
+  {
+    id: 1,
+    siteName: "MEDIATEK APPLICATION",
+    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+    description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
+    siteLink: "https://mtk-genuine.com/",
+  },
+];
+
+export default siteOptions;
