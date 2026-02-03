c641dcfa1813890341eb6008ef74856e42ce85f9
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:38:54 +0800

    Update footer_details.js

--- src/components/footer_details.js
+++ src/components/footer_details.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const FooterDetails = () => {
+const FooterDetails = ({ siteName = "MEDIATEK VPN" }) => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h2 className="footer-title">MEDIATEK VPN</h2>
+        <h2 className="footer-title">{siteName}</h2>
         <p className="footer-copy">&copy; 2024 Dexter Eskalarte. All rights reserved.</p>
       </div>
     </footer>
