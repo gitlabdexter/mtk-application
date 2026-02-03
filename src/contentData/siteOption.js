1e5c93fd4f11dff3adabb6c82ab4cfd2f134cd79
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:51:40 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -1,12 +1,11 @@
-// src/contentData/siteOption.js
-const siteOptions = [
-  {
-    id: 1,
-    siteName: "MEDIATEK APPLICATION",
-    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
-    description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
-    siteLink: "https://mtk-genuine.com/",
-  },
-];
-
-export default siteOptions;
+const siteOptionData = [
+    {
+      id: 1,
+      siteName: "MEDIATEK APPLICATION",
+      image:
+        "https://i.ibb.co/jPnXFqj5/icon-icon.png",
+      description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
+      siteLink: "https://sumsag-ultra.site/"
+    }
+  ];
+  export default siteOptionData;
