f54155465de7397a46680f640823a2a5431fb647
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:08:41 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -29,7 +29,7 @@ const productData = [
       rating: 5,
       timeLeft: 24,
       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/MediatekOvpn.apk"
-        },
+    },
     {
       id: 4,
       name: "MTK DITO APP",
@@ -40,16 +40,6 @@ const productData = [
       timeLeft: 24,
       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ditomtk.apk"
     }
-   // {
-   //   id: 3,
-   //   name: "STARLINK VPN LOGIN",
-   //   image:
-   //     "https://i.ibb.co/NFWPLbQ/app-main-logo.png",
-   //   productType: "openBackHeadphones",
-   //   description: ["OVPN", "UDP", "V2RAY"],
-   //   rating: 5,
-   //   timeLeft: 27,
-   //   appLink: "https://play.google.com/store/apps/details?id=app.fixbuild.StarlinkVPNv2"
-   // }
+
   ];
   export default productData;
