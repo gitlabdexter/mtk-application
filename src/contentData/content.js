705a98c5fbafe1f6bf4d369c48da299384401d1f
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:27:20 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,82 +1,50 @@
-import { useState, useEffect } from "react";
-import { FaStar } from "react-icons/fa";
+const productData = [
+  {
+    id: 1,
+    name: "MTK GENUINE PRO",
+    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+    rating: 5,
+    timeLeft: 27,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/progenuine.apk",
+  },
+  {
+    id: 2,
+    name: "MTK TUNNEL V24",
+    image: "https://i.ibb.co/7t6f2F7s/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkv24.apk",
+    // appLink:
+    //   "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkgenuine-release1.apk",
+  },
+  {
+    id: 3,
+    name: "MEDIATEK OVPN",
+    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/MediatekOvpn.apk",
+  },
+  {
+    id: 4,
+    name: "MTK DITO APP",
+    image: "https://i.ibb.co/dsBK8FTw/app-main-logo.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ditomtk.apk",
+  },
+];
 
-export function Products({
-  id,
-  image,
-  name,
-  description,
-  appLink,
-  rating,
-  timeLeft, // added and will be used
-}) {
-  const [downloads, setDownloads] = useState(0);
-
-  // Load downloads from localStorage
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    const saved = localStorage.getItem(`downloads_${id}`);
-    if (saved) setDownloads(Number(saved));
-  }, [id]);
-
-  // Save downloads to localStorage
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    localStorage.setItem(`downloads_${id}`, downloads);
-  }, [id, downloads]);
-
-  const handleDownload = () => {
-    setDownloads((prev) => prev + 1);
-    window.open(appLink, "_blank", "noopener,noreferrer");
-  };
-
-  return (
-    <div className="productList">
-      <div className="productCard">
-        <img
-          src={image}
-          alt={`${name} app`}
-          className="productImage"
-        />
-
-        <div className="productCard__content">
-          <h3 className="productName">{name}</h3>
-
-          <div className="displayStack__1">
-            <select className="productPrice">
-              {description.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-
-            <div className="productRating">
-              {[...Array(rating)].map((_, index) => (
-                <FaStar key={index} />
-              ))}
-            </div>
-          </div>
-
-          <div className="displayStack__2">
-            <button
-              className="productTime"
-              onClick={handleDownload}
-            >
-              DOWNLOAD
-            </button>
-
-            {/* Added this line to use timeLeft */}
-            <p className="time-left">{timeLeft} days left</p>
-
-            <p className="download-text">
-              Client Downloads: {downloads}
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+export default productData;
