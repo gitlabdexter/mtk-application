07894291b9646cb2e495ed9af0550c66eb7feab1
gitlabdexter <gitlabdexter@gmail.com>
2026-02-03 13:12:44 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,45 +1,50 @@
 const productData = [
-    {
-      id: 1,
-      name: "MTK GENUINE PRO",
-      image:"https://i.ibb.co/qLsDNCVN/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 27,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/progenuine.apk"
-    },
-    {
-      id: 2,
-      name: "MTK TUNNEL V24",
-      image:"https://i.ibb.co/7t6f2F7s/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkv24.apk"
-      //appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkgenuine-release1.apk"
-    },
-    {
-      id: 3,
-      name: "MEDIATEK OVPN",
-      image:"https://i.ibb.co/qLsDNCVN/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/MediatekOvpn.apk"
-    },
-    {
-      id: 4,
-      name: "MTK DITO APP",
-      image:"https://i.ibb.co/dsBK8FTw/app-main-logo.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN"],
-      rating: 5,
-      timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ditomtk.apk"
-    }
+  {
+    id: 1,
+    name: "MTK GENUINE PRO",
+    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+    rating: 5,
+    timeLeft: 27,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/progenuine.apk",
+  },
+  {
+    id: 2,
+    name: "MTK TUNNEL V24",
+    image: "https://i.ibb.co/7t6f2F7s/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkv24.apk",
+    // appLink:
+    //   "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/mtkgenuine-release1.apk",
+  },
+  {
+    id: 3,
+    name: "MEDIATEK OVPN",
+    image: "https://i.ibb.co/qLsDNCVN/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/MediatekOvpn.apk",
+  },
+  {
+    id: 4,
+    name: "MTK DITO APP",
+    image: "https://i.ibb.co/dsBK8FTw/app-main-logo.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN"],
+    rating: 5,
+    timeLeft: 24,
+    appLink:
+      "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ditomtk.apk",
+  },
+];
 
-  ];
-  export default productData;
+export default productData;
